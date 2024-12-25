736a9a9b8adc9ebd159e3f6c871c170e51dba5e2
Josh Parsons <jparsons04@gmail.com>
2024-12-25 14:42:29 -0800

    Add Advent of Code 2024 Day 6 Part 1

---
+++ 2024/day6-part1.py
@@ -0,0 +1,78 @@
+room = []
+guard_directions = ['^', '>', 'v', '<']
+guardRow = 0
+guardCol = 0
+guardDir = ''
+
+with open('day6-input.txt', 'r') as f:
+    for line in f:
+        room_row = []
+        for _, rr in enumerate(line.strip()):
+            room_row.append(rr)
+        room.append(room_row)
+
+
+def move_guard(guardRow, guardCol, guardDir):
+    if guardDir == '^':
+        room[guardRow-1][guardCol] = '^'
+        return guardRow-1, guardCol
+    elif guardDir == '>':
+        room[guardRow][guardCol+1] = '>'
+        return guardRow, guardCol+1
+    elif guardDir == 'v':
+        room[guardRow+1][guardCol] = 'v'
+        return guardRow+1, guardCol
+    elif guardDir == '<':
+        room[guardRow][guardCol-1] = '<'
+        return guardRow, guardCol-1
+
+
+def check_rotate(guardRow, guardCol, guardDir):
+    if guardDir == '^':
+        # not on top edge of room
+        if guardRow != 0:
+            if room[guardRow-1][guardCol] == '#':
+                return '>'
+        else:
+            return 'out'
+    elif guardDir == '>':
+        # not on right edge of room
+        if guardCol != len(room[0])-1:
+            if room[guardRow][guardCol+1] == '#':
+                return 'v'
+        else:
+            return 'out'
+    elif guardDir == 'v':
+        # not on bottom edge of room
+        if guardRow != len(room)-1:
+            if room[guardRow+1][guardCol] == '#':
+                return '<'
+        else:
+            return 'out'
+    elif guardDir == '<':
+        # not on left edge of room
+        if guardCol != 0:
+            if room[guardRow][guardCol-1] == '#':
+                return '^'
+        else:
+            return 'out'
+    return guardDir
+
+
+if __name__ == "__main__":
+    for row, rr in enumerate(room):
+        for col, _ in enumerate(rr):
+            if room[row][col] in guard_directions:
+                guardRow = row
+                guardCol = col
+                guardDir = room[row][col]
+
+    while guardDir != 'out':
+        guardDir = check_rotate(guardRow, guardCol, guardDir)
+
+        room[guardRow][guardCol] = 'X'
+
+        if guardDir != 'out':
+            guardRow, guardCol = move_guard(guardRow, guardCol, guardDir)
+
+    print(sum(x.count('X') for x in room))
