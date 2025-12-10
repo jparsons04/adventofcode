1adc7d28ba5d6f56b69b5140d7f4184082eda7cf
Josh Parsons <joshparsons@honeycomb.io>
2025-12-09 22:05:48 -0800

    Pushing a partial solution for day 08

--- 2025/day08.go
+++ 2025/day08.go
@@ -19,10 +19,14 @@ type JunctionBoxPos struct {
 }
 
 type JunctionBox struct {
-	Position                     JunctionBoxPos
-	ClosestJunctionBox           JunctionBoxPos
-	DistanceToClosestJunctionBox float64
-	Connected                    bool
+	Position JunctionBoxPos
+}
+
+type JunctionBoxPair struct {
+	JunctionBox1 JunctionBoxPos
+	JunctionBox2 JunctionBoxPos
+	Distance     float64
+	Connected    bool
 }
 
 func main() {
@@ -38,6 +42,7 @@ func main() {
 	sc := bufio.NewScanner(f)
 
 	junctionBoxes := make(map[JunctionBoxPos]JunctionBox)
+	var junctionBoxPairs []JunctionBoxPair
 
 	for sc.Scan() {
 		line := strings.Split(sc.Text(), ",")
@@ -54,118 +59,118 @@ func main() {
 	}
 
 	for pos1 := range junctionBoxes {
-		currentJunctionBox := junctionBoxes[pos1]
-
-		var closestJunctionBox JunctionBoxPos
-		closestDistance := float64(-1)
-
-		// Get the shortest straight-line distance from the junction box at pos1
-		// to all other junction boxes
 		for pos2 := range junctionBoxes {
 			if pos1 != pos2 {
 				x := pos1.X - pos2.X
 				y := pos1.Y - pos2.Y
 				z := pos1.Z - pos2.Z
-
 				distance := math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2) + math.Pow(z, 2))
-
-				if closestDistance == -1 || distance < closestDistance {
-					closestDistance = distance
-					closestJunctionBox = pos2
-				}
+				junctionBoxPairs = append(junctionBoxPairs, JunctionBoxPair{
+					JunctionBox1: pos1,
+					JunctionBox2: pos2,
+					Distance:     distance,
+					Connected:    false,
+				})
 			}
 		}
-
-		currentJunctionBox.ClosestJunctionBox = closestJunctionBox
-		currentJunctionBox.DistanceToClosestJunctionBox = closestDistance
-
-		junctionBoxes[pos1] = currentJunctionBox
 	}
 
-	var sortedJunctionBoxes []JunctionBox
-
-	for _, v := range junctionBoxes {
-		sortedJunctionBoxes = append(sortedJunctionBoxes, v)
-	}
-
-	sort.Slice(sortedJunctionBoxes, func(i, j int) bool {
-		return sortedJunctionBoxes[i].DistanceToClosestJunctionBox < sortedJunctionBoxes[j].DistanceToClosestJunctionBox
+	sort.Slice(junctionBoxPairs, func(i, j int) bool {
+		return junctionBoxPairs[i].Distance < junctionBoxPairs[j].Distance
 	})
 
-	for _, v := range sortedJunctionBoxes {
-		fmt.Printf("%+v\n", v)
-	}
-
 	var circuits [][]JunctionBoxPos
+	var numberofConnections int
 
-	for i := range 10 {
+	for i := range len(junctionBoxPairs) {
 		var circuit []JunctionBoxPos
 
-		fmt.Printf("Evaluating: %+v\n", sortedJunctionBoxes[i])
-		junctionBoxPos1 := sortedJunctionBoxes[i].Position
-		junctionBoxPos2 := sortedJunctionBoxes[i].ClosestJunctionBox
+		//fmt.Printf("Evaluating: %+v\n", junctionBoxPairs[i])
+		junctionBoxPos1 := junctionBoxPairs[i].JunctionBox1
+		junctionBoxPos2 := junctionBoxPairs[i].JunctionBox2
 
-		if !junctionBoxes[junctionBoxPos1].Connected || !junctionBoxes[junctionBoxPos2].Connected {
-			junctionBoxPos1Idx := slices.IndexFunc(sortedJunctionBoxes, func(j JunctionBox) bool { return j.Position == junctionBoxPos1 })
-			junctionBoxPos2Idx := slices.IndexFunc(sortedJunctionBoxes, func(j JunctionBox) bool { return j.Position == junctionBoxPos2 })
-
+		if !junctionBoxPairs[i].Connected {
 			// connect the two junction boxes together
-			sortedJunctionBoxes[junctionBoxPos1Idx].Connected = true
-			sortedJunctionBoxes[junctionBoxPos2Idx].Connected = true
+			junctionBoxPairs[i].Connected = true
 
-			fmt.Printf("After connection\n%+v\n%+v\n", sortedJunctionBoxes[junctionBoxPos1Idx], sortedJunctionBoxes[junctionBoxPos2Idx])
-
-			circuitIdx1, circuitIdx2 := -1, -1
-
-			// look for pos1 in circuits
-			for _, c := range circuits {
-				circuitIdx1 = slices.IndexFunc(c, func(j JunctionBoxPos) bool { return j == junctionBoxPos1 })
-				//fmt.Printf("circuitIdx1 returned %d\n", circuitIdx1)
-				if circuitIdx1 != -1 {
+			// Also mark the reciprocal pair as connected
+			for j := range junctionBoxPairs {
+				if junctionBoxPairs[j].JunctionBox1 == junctionBoxPos2 &&
+					junctionBoxPairs[j].JunctionBox2 == junctionBoxPos1 {
+					junctionBoxPairs[j].Connected = true
 					break
 				}
 			}
 
-			for _, c := range circuits {
+			circuitIdx1, circuitIdx2 := -1, -1
+
+			// look for pos1 in circuits
+			for i, c := range circuits {
+				circuitIdx1 = slices.IndexFunc(c, func(j JunctionBoxPos) bool { return j == junctionBoxPos1 })
+				if circuitIdx1 != -1 {
+					// Capture the outer index of the circuit that pos1 is in
+					circuitIdx1 = i
+					break
+				}
+			}
+
+			for i, c := range circuits {
 				circuitIdx2 = slices.IndexFunc(c, func(j JunctionBoxPos) bool { return j == junctionBoxPos2 })
-				//fmt.Printf("circuitIdx2 returned %d\n", circuitIdx2)
 				if circuitIdx2 != -1 {
+					// Capture the outer index of the circuit that pos2 is in
+					circuitIdx2 = i
 					break
 				}
 			}
 
 			if circuitIdx1 == -1 && circuitIdx2 == -1 {
-
 				// Neither in an existing circuit, create a new one
 				circuit = append(circuit, junctionBoxPos1, junctionBoxPos2)
 				circuits = append(circuits, circuit)
 			} else if circuitIdx1 != -1 && circuitIdx2 == -1 {
 				// pos1 found but not pos2, add pos2 to pos1's circuit
 				existingCircuit := circuits[circuitIdx1]
-
-				// briefly remove from circuits
-				circuits = append(circuits[:circuitIdx1], circuits[circuitIdx1+1:]...)
-
 				existingCircuit = append(existingCircuit, junctionBoxPos2)
-
-				// add it back to circuits
-				circuits = append(circuits, existingCircuit)
-
+				circuits[circuitIdx1] = existingCircuit
 			} else if circuitIdx1 == -1 && circuitIdx2 != -1 {
 				// pos2 found but not pos1, add pos1 to pos2's circuit
 				existingCircuit := circuits[circuitIdx2]
-
-				// briefly remove from circuits
-				circuits = append(circuits[:circuitIdx2], circuits[circuitIdx1+2:]...)
-
 				existingCircuit = append(existingCircuit, junctionBoxPos1)
+				circuits[circuitIdx2] = existingCircuit
+			} else {
+				circuit1 := circuits[circuitIdx1]
+				circuit2 := circuits[circuitIdx2]
 
-				// add it back to circuits
-				circuits = append(circuits, existingCircuit)
+				// If both are found in two different circuits, merge the two circuits
+				// otherwise do nothing
+				if !slices.Equal(circuit1, circuit2) {
+					// Need to handle circuit index carefully - if circuitIdx1 > circuitIdx2,
+					// after removing circuitIdx2, circuitIdx1 shifts down by 1
+					if circuitIdx1 > circuitIdx2 {
+						// remove the second circuit from circuits
+						circuits = append(circuits[:circuitIdx2], circuits[circuitIdx2+1:]...)
+						// add the second circuit to the first circuit
+						circuit1 = append(circuit1, circuit2...)
+						// Update the merged circuit (index shifted down by 1)
+						circuits[circuitIdx1-1] = circuit1
+					} else {
+						// remove the second circuit from circuits
+						circuits = append(circuits[:circuitIdx2], circuits[circuitIdx2+1:]...)
+						// add the second circuit to the first circuit
+						circuit1 = append(circuit1, circuit2...)
+						// Update the merged circuit in the circuits slice
+						circuits[circuitIdx1] = circuit1
+					}
+				}
 			}
 
-			// if both junction boxes are found, they should both be in the same circuit already
-			// in which case we do nothing
+			// Count every connection attempt, not just successful ones
+			numberofConnections++
+		}
+
+		// Stop after 1000 connections
+		if numberofConnections == 1000 {
+			break
 		}
 	}
 
@@ -173,9 +178,5 @@ func main() {
 		return len(circuits[i]) > len(circuits[j])
 	})
 
-	for i := range circuits {
-		fmt.Printf("%d ", len(circuits[i]))
-	}
-
-	fmt.Printf("\n%+v\n", circuits)
+	fmt.Printf("Part one: Product of the three largest circuits: %d\n", len(circuits[0])*len(circuits[1])*len(circuits[2]))
 }
